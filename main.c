8221af29d1dba28e34ec9f58e20fb0ec56397d20
sorakann iMac <ski@student.42lausanne.com>
2022-02-04 18:08:54 +0100

    amelioration

--- main.c
+++ main.c
@@ -29,13 +29,13 @@ int main(void)
 	data.coef = 1;
 
 	// data.file.name = "test_maps/texte";
-	// data.file.name = "test_maps/42.fdf";	
+	data.file.name = "test_maps/42.fdf";	
 	// data.file.name = "test_maps/100-6.fdf"; 		// depasse peut etre gauche et droite
 
 	// data.file.name = "test_maps/elem-fract.fdf";		// TRES LONG
 	// data.file.name = "test_maps/julia.fdf";			// TRES LONG
 
-	data.file.name = "test_maps/mars.fdf";			// PROBLEME
+	// data.file.name = "test_maps/mars.fdf";			// PROBLEME
 	data.xyz = get_data_from_file(&(data.file), &(data.info_table));
 	if (data.xyz  == NULL)
 		return (0);
@@ -54,6 +54,9 @@ int main(void)
 	init_vector_unit_iso(&data);
 	init_vector_zoom(&data, data.coef);
 	init_vector_decal(&data);
+
+
+		rendering(data.xyz, &data);
 
 	/* GESTION des événements -------------------------------------------- */
 	mlx_mouse_hook(data.win_ptr, action_mouse, (void *)&data);
